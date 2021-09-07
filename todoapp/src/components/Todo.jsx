af857e7ee1b6efc3530c430d246c001073d0bda9
Diseyi <filomiinaa@gmail.com>
2021-09-07 19:08:08 +0100

    changed the background colour

--- todoapp/src/components/Todo.jsx
+++ todoapp/src/components/Todo.jsx
@@ -7,23 +7,11 @@ const Todo = ({text, todo, setTodo, id, todos}) => {
         setTodo(todo.filter(el => el.id !== todos.id))
     }
 
-//     const handleComplete = () => {
-// setTodo(todo.map((item) => {
-//     if(item.id === todos.id) {
-//         return {
-//             ...item, completed : !item.completed
-//         }
-//     }
-// return item
-// }))
-// console.log({todos});
-
-//     }
-
     return (
         <div>
             <div className="todo"  > 
             <li className="todo-li" > {text} </li>
+            <button className="edit-button" >edit</button>
             <button className="delete-button" onClick={handleDelete} >delete</button>
             </div>
         </div>
